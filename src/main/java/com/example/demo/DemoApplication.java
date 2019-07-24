8693f7078e95599adca2d03eebfe383ffd7c9e6f
zhenglongfei3 <zhenglongfei3@jd.com>
2019-07-24 18:11:09 +0800

    xxx

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableElasticJob
-@ComponentScan(basePackages = {"com.cxytiandi"})
+//@ComponentScan(basePackages = {"com.cxytiandi"})
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/job/TestJob.java
@@ -0,0 +1,25 @@
+package com.example.demo.job;
+
+import com.cxytiandi.elasticjob.annotation.ElasticJobConf;
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhenglongfei on 2019/7/22
+ *
+ * @VERSION 1.0
+ */
+@Component
+@Slf4j
+@ElasticJobConf(name = "dayJob", cron = "0/10 * * * * ?", shardingTotalCount = 2,
+        shardingItemParameters = "0=AAAA,1=BBBB", description = "简单任务", failover = true)
+public class TestJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        log.info("TestJob任务名：【{}】, 片数：【{}】, param=【{}】", shardingContext.getJobName(), shardingContext.getShardingTotalCount(),
+                shardingContext.getShardingParameter());
+    }
+}
